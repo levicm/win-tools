54ba78ce60f4fb1ca54b288c70f87ae385c35e0a
Levi Mota <levi.mota@gmail.com>
2023-08-09 23:11:59 -0300

    Organizing commands

---
+++ win-all.ps1
@@ -0,0 +1,17 @@
+Write-Host "Installing Wiget..."
+iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/install/install-winget.ps1);
+
+Write-Host "Debloat..."
+iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/win-debloat.ps1);
+
+Write-Host "Essential tweaks..."
+iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/win-tweaks.ps1);
+
+Write-Host "Customize..."
+iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/win-customize.ps1);
+
+Write-Host "Windows optional features..."
+iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/win-enable-features.ps1);
+
+Write-Host "Installing Apps..."
+iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/win-install-apps.ps1);
--- win-install-apps.ps1
+++ win-install-apps.ps1
@@ -1,21 +1,6 @@
 Write-Host "Installing Chocolatey..."
 iex (irm https://chocolatey.org/install.ps1);
 
-Write-Host "Installing Wiget..."
-iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/install/install-winget.ps1);
-
-Write-Host "Debloat..."
-iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/win-debloat.ps1);
-
-Write-Host "Essential tweaks..."
-iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/win-tweaks.ps1);
-
-Write-Host "Customize..."
-iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/win-customize.ps1);
-
-Write-Host "Windows optional features..."
-iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/win-enable-features.ps1);
-
 Write-Host "Installing Essential Apps..."
 iex (irm https://raw.githubusercontent.com/levicm/win-tools/master/install/install-essential-apps.ps1);
 
