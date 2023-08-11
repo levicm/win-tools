b74793c2f5fd901f77a312540898323fe4b1b184
Levi Mota <levi.mota@gmail.com>
2023-08-10 22:06:19 -0300

    Replacing choco by winget

--- install/install-dev-apps.ps1
+++ install/install-dev-apps.ps1
@@ -1,5 +1,5 @@
-choco install -y git
-choco install -y vscode
-choco install -y openjdk11
+winget install Git.Git --disable-interactivity --silent
+winget install Microsoft.VisualStudioCode --disable-interactivity --silent
+winget install Python.Python.3.10 --disable-interactivity --silent
+winget install ojdkbuild.openjdk.17.jdk --disable-interactivity --silent
 choco install -y eclipse
-choco install -y python3
