706a90978e0476f95ba689c619f7a1f5ed54543d
Levi Mota <levi.mota@gmail.com>
2023-11-12 14:37:38 -0300

    Adding notion

--- install/install-office-apps.ps1
+++ install/install-office-apps.ps1
@@ -2,3 +2,4 @@ winget install Microsoft.Office --disable-interactivity --silent
 winget install TheDocumentFoundation.LibreOffice --disable-interactivity --silent
 winget install AcroSoftware.CutePDFWriter --disable-interactivity --silent
 winget install PDFsam.PDFsam --disable-interactivity --silent
+winget install notion.notion --disable-interactivity --silent
