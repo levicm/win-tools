165a3f70cb83a96f2463edae16da2c060c0058ad
Levi Mota <levi.mota@gmail.com>
2024-05-26 21:31:14 -0300

    Update install-everything-on-powertoys.ps1

--- install/install-everything-on-powertoys.ps1
+++ install/install-everything-on-powertoys.ps1
@@ -1,9 +1,3 @@
-# URL and Destination
-$pluginUrl = "https://github.com/lin-ycv/EverythingPowerToys/releases/download/v0.80.0/EverythingPT-0.80.0-x64.zip"
-# Extract path
-$extractPath = "c:\temp\"
-# Get zipfile name from source URL
-$zipFile = $extractPath + $(Split-Path -Path $pluginUrl -Leaf)
 # Everything path
 $everythingPath = "C:\Program Files\Everything\"
 # Powertoys path
@@ -28,36 +22,5 @@ if (Test-Path -Path $everythingPath) {
     winget install voidtools.Everything --disable-interactivity --silent
 }
 
-# Create temp destination for the zip
-if (!(Test-Path -Path $extractPath)) {
-    mkdir $extractPath
-}
-
-# Download file
-Write-Host "Downloading file..."
-Invoke-WebRequest -Uri $pluginUrl -OutFile $zipFile
-
-Write-Host "Extracting file..."
-# Create instance of COM Object
-$objShell = New-Object -ComObject Shell.Application
-# Extract the Files
-$extractedFiles = $ObjShell.NameSpace($zipFile).Items()
-# Copy the extracted files to the destination folder
-$ObjShell.NameSpace($extractPath).CopyHere($extractedFiles)
-
-if (Test-Path -Path $powertoysLocalPath) {
-    $powertoysPath = $powertoysLocalPath
-}
-
-if (Test-Path -Path $powertoysPath) {
-    # Powertoys plugins path
-    $powertoysPluginsPath = $powertoysPath + "RunPlugins\"
-    $pluginFolderName = "Everything"
-    $sourceFolder = $extractPath + $pluginFolderName
-    $destFolder = $powertoysPluginsPath + $pluginFolderName
-    
-    Write-Host "Coping files from " $sourceFolder "to" $destFolder "..."
-    Copy-Item -Path $sourceFolder -Destination $destFolder -Recurse -Force
-} else {
-    Write-Host "PowerToys path not found!!"
-}
+# Install Everything PowerToys Plugin
+winget install lin-ycv.EverythingPowerToys --disable-interactivity --silent
