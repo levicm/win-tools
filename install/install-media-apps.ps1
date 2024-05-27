3078b3b6cff237b4a0583e723db775022d4920f7
Levi Mota <levi.mota@gmail.com>
2024-05-26 21:48:10 -0300

    Update install-media-apps.ps1

--- install/install-media-apps.ps1
+++ install/install-media-apps.ps1
@@ -1,4 +1,6 @@
 winget install Spotify.Spotify --disable-interactivity --silent
-winget install WhatsApp.WhatsApp --disable-interactivity --silent
+#winget install WhatsApp.WhatsApp --disable-interactivity --silent
+# WhatsApp of winget repo seams to be broke, using MS Store repo:
+winget install 9NKSQGP7F2NH --disable-interactivity --silent
 winget install Telegram.TelegramDesktop --disable-interactivity --silent
 winget install VideoLAN.VLC --disable-interactivity --silent
