164758bf8d0311105555bb5c9a043c3d48f54355
Levi Mota <levi.mota@gmail.com>
2024-06-19 09:55:36 -0300

    Including a more robust verification and installing from Github

--- install/install-winget.ps1
+++ install/install-winget.ps1
@@ -1,13 +1,31 @@
 Write-Host "Checking winget..."
+[bool] $doInstall = $true
 # Check if winget is installed
-if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe){
-    'Winget Already Installed'
-} else {
-    # Installing winget from the Microsoft Store
-    Write-Host "Winget not found, installing it now."
-    Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
-    $nid = (Get-Process AppInstaller).Id
-    Wait-Process -Id $nid
-    Write-Host Winget Installed
+if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe) {
+    Write-Host "Winget Already Installed"
+    Write-Host "Checking if it is working..."
+    winget upgrade
+    if ($LastExitCode -eq 0) {
+        Write-Host "Winget is ok"
+        $doInstall = $false
+    } else {
+        Write-Host "Winget is installed but is not working well"
+    }
 }
-
+if ($doInstall) {
+    # Installing winget from the Github...
+    Write-Host "Winget not found or not working, installing it..."
+    # get latest download url
+    $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+    $URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+            Select-Object -ExpandProperty "assets" |
+            Where-Object "browser_download_url" -Match '.msixbundle' |
+            Select-Object -ExpandProperty "browser_download_url"
+    # download
+    Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
+    # install
+    Add-AppxPackage -Path "Setup.msix"
+    # delete file
+    Remove-Item "Setup.msix"
+    Write-Host "Winget Installed!"
+}
